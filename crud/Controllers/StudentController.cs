0a62c7103223790a327a0d898e20b3988bf1bb5a
thsurajsinghrajput <thsurajsinghrajput@gmail.com>
2023-02-26 16:03:43 +0530

    student controller created

---
+++ crud/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace crud.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
